754c4c60a40b89b0f562249c967f703068a5b29a
John Ballesteros <johnshift.dev@gmail.com>
2021-09-24 00:52:28 +0800

    basic param get request

---
+++ src/main/java/dev/johnshift/udemyspringbootfundamentals/controllers/Greeting.java
@@ -0,0 +1,26 @@
+package dev.johnshift.udemyspringbootfundamentals.controllers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Greeting {
+  private long id;
+  private String message;
+
+  public long getId() {
+    return this.id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getMessage() {
+    return this.message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+}
