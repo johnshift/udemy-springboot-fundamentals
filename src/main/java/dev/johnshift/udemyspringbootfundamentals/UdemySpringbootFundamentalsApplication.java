23844060b7034d9b58b226f6f582a3325de873b6
John Ballesteros <johnshift.dev@gmail.com>
2021-09-24 00:13:42 +0800

    springboot init

---
+++ src/main/java/dev/johnshift/udemyspringbootfundamentals/UdemySpringbootFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package dev.johnshift.udemyspringbootfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringbootFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringbootFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/johnshift/udemyspringbootfundamentals/UdemySpringbootFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johnshift.udemyspringbootfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringbootFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
