2768a6cb54d16336025c9f36efebc97d7273c526
John Ballesteros <johnshift.dev@gmail.com>
2021-09-24 12:20:47 +0800

    addBook http response

--- src/main/java/dev/johnshift/udemyspringbootfundamentals/controllers/BooksCtl.java
+++ src/main/java/dev/johnshift/udemyspringbootfundamentals/controllers/BooksCtl.java
@@ -1,6 +1,9 @@
 package dev.johnshift.udemyspringbootfundamentals.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +16,25 @@ public class BooksCtl {
   @Autowired
   BooksRepository repository;
 
+  @Autowired
+  AddBookResponse addResponse;
+
   @PostMapping("/add-book")
-  public void addBook(@RequestBody Books book) {
+  public ResponseEntity<AddBookResponse> addBook(@RequestBody Books book) {
 
     // set id for new book
     book.setId(book.getIsbn() + book.getAisle());
     repository.save(book);
+
+    // set json response
+    addResponse.setMessage("Book has been successfully added");
+    addResponse.setId(book.getId());
+
+    // set headers
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("unique", book.getId());
+
+    // build http response
+    return new ResponseEntity<AddBookResponse>(addResponse, headers, HttpStatus.CREATED);
   }
 }
