d19dd8ea4ad55614b847c6777ec592d6ab840903
John Ballesteros <johnshift.dev@gmail.com>
2021-09-25 09:41:58 +0800

    unit test using mockmvc

--- src/test/java/dev/johnshift/udemyspringbootfundamentals/UdemySpringbootFundamentalsApplicationTests.java
+++ src/test/java/dev/johnshift/udemyspringbootfundamentals/UdemySpringbootFundamentalsApplicationTests.java
@@ -2,13 +2,23 @@ package dev.johnshift.udemyspringbootfundamentals;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.test.web.servlet.MockMvc;
 
 import dev.johnshift.udemyspringbootfundamentals.books.Book;
 import dev.johnshift.udemyspringbootfundamentals.books.BooksController;
@@ -17,6 +27,7 @@ import dev.johnshift.udemyspringbootfundamentals.books.BooksService;
 import dev.johnshift.udemyspringbootfundamentals.books.ResponseAddBook;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UdemySpringbootFundamentalsApplicationTests {
 
   @Autowired
@@ -27,6 +38,9 @@ class UdemySpringbootFundamentalsApplicationTests {
 
   @MockBean
   BooksService booksService;
+
+  @Autowired
+  MockMvc mockMvc;
 
   @Test
   void contextLoads() {
@@ -41,6 +55,8 @@ class UdemySpringbootFundamentalsApplicationTests {
     when(booksService.buildId(book.getIsbn(), book.getAisle())).thenReturn(book.getId());
     // mock bookService.bookExists
     when(booksService.bookExists(book.getId())).thenReturn(false);
+    // mock booksRepository.save
+    when(booksRepository.save(any())).thenReturn(book);
 
     // test http response
     ResponseEntity<ResponseAddBook> response = booksController.addBook(createBook());
@@ -50,6 +66,24 @@ class UdemySpringbootFundamentalsApplicationTests {
     ResponseAddBook responseBody = response.getBody();
     assertEquals(book.getId(), responseBody.id);
     assertEquals("Book has been successfully added", responseBody.message);
+
+  }
+
+  @Test
+  public void addBookControllerTest() throws Exception {
+
+    // mockito
+    Book book = createBook();
+    ObjectMapper map = new ObjectMapper();
+    String jsonPayload = map.writeValueAsString(book);
+    when(booksService.buildId(book.getIsbn(), book.getAisle())).thenReturn(book.getId());
+    when(booksService.bookExists(book.getId())).thenReturn(false);
+    when(booksRepository.save(any())).thenReturn(book);
+
+    // mockmvc
+    this.mockMvc.perform(post("/books").contentType(MediaType.APPLICATION_JSON).content(jsonPayload))
+        .andExpect(status().isCreated()).andExpect(jsonPath("$.id").value(book.getId()))
+        .andExpect(jsonPath("$.message").value("Book has been successfully added"));
 
   }
 
